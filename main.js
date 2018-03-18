db0395d005e92d23a6411c573a6d443f984187a0
densedever <dylanmrczk243@gmail.com>
2018-03-18 18:49:04 -0500

    Added functionality to nav bar

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 
+const navLinks = document.querySelectorAll('#main-nav a');
+
+
+for(let i=0, j=navLinks.length; i<j; i++) {
+  navLinks[i].addEventListener('click', navHandler);
+}
+
+function navHandler(event) {
+  console.log('works');
+  for(let i=0, j=navLinks.length; i<j; i++) {
+    navLinks[i].setAttribute('class', '');
+  }
+  this.setAttribute('class', 'active');
+}
 
