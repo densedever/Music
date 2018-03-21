ffa639023bf5e4ea0e4ab9750ec6383dfb70e72d
densedever <dylanmrczk243@gmail.com>
2018-03-21 16:47:25 -0500

    Additional content

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 
 const navLinks = document.querySelectorAll('#main-nav a');
-
+const sections = document.querySelectorAll('.read');
 
 for(let i=0, j=navLinks.length; i<j; i++) {
   navLinks[i].addEventListener('click', navHandler);
 }
 
 function navHandler(event) {
-  console.log('works');
   for(let i=0, j=navLinks.length; i<j; i++) {
     navLinks[i].setAttribute('class', '');
   }
+  for(let i=0, j=sections.length; i<j; i++) {
+    // set all to hidden
+    sections[i].setAttribute('class', sections[i].className.replace(sections[i].className, 'read'))
+  }
+  // show current
+  
   this.setAttribute('class', 'active');
 }
 
-// TODO: include function for showing/hiding individual sections ('.rolling-content').
+// TODO: 
+// include function for showing/hiding individual sections ('.rolling-content').
+// show/hide main sections based on navbar clicks.
