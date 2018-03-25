f1ef503f61a026566f75ddc0690de1b95ec7bd30
densedever <dylanmrczk243@gmail.com>
2018-03-24 19:04:34 -0500

    Added some JS

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 const navLinks = document.querySelectorAll('#main-nav a');
-const sections = document.querySelectorAll('.read');
+const sections = document.querySelectorAll('.navbarshowhide');
 
 for(let i=0, j=navLinks.length; i<j; i++) {
   navLinks[i].addEventListener('click', navHandler);
@@ -12,13 +12,16 @@ function navHandler(event) {
   }
   for(let i=0, j=sections.length; i<j; i++) {
     // set all to hidden
-    sections[i].setAttribute('class', sections[i].className.replace(sections[i].className, 'read'))
+    sections[i].setAttribute('class', sections[i].className.replace(sections[i].className, 'navbarshowhide'))
   }
   // show current
-  
+
   this.setAttribute('class', 'active');
 }
 
 // TODO: 
 // include function for showing/hiding individual sections ('.rolling-content').
 // show/hide main sections based on navbar clicks.
+
+
+navLinks[0].setAttribute('class', 'active');
